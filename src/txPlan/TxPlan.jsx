2497a75a644706a9d252b302922ff2cfd063fbd9
tbohn2 <tanner.bohn@yahoo.com>
2023-12-19 14:38:32 -0700

    Created function to create PDF of tx plan

--- src/txPlan/TxPlan.jsx
+++ src/txPlan/TxPlan.jsx
@@ -1,4 +1,20 @@
+import React, { useRef } from 'react';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+
 const TxPlan = (txArray) => {
+
+
+    const generatePdf = async () => {
+        const pdfDocument = new jsPDF();
+        const txPlanRef = useRef(null);
+        const canvas = await html2canvas(txPlanRef.current);
+
+        pdfDocument.addImage(canvas.toDataURL('image/png'), 'PNG', 10, 10, 190, 0);
+
+        pdfDocument.save('TxPlan.pdf');
+    };
+
     return (
         <div className="col-12">
             {txArray.map((tx, index) => {
